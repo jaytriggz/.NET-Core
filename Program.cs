f476b8ad2ee12572ca0915133223582e4bdbb8d0
Dev-triggz <arotroy01@hotmail.com>
2025-03-13 12:14:42 -0400

    #C core

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+for (int i=0; i <= 2; ++i)
+{
+    Console.WriteLine("Outer: " + i);
+}
+
+for(int j=0; j <= 3; j++)
+{
+    Console.WriteLine("inner: " + j);
+}
